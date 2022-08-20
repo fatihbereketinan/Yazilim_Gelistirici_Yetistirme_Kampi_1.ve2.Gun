0996ae57881709d337365c6d5607b42a7f948001
fatihbereketinan <fatihbereketinan@gmail.com>
2022-08-20 16:10:12 +0300

    Yazilim_Gelistirici_Yetistirme_Kampi_1.ve2.Gun

---
+++ Donguler/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+            string kurs4 = "Python";
+            string kurs5 = "C#";
+
+            Console.WriteLine(kurs1);
+            Console.WriteLine(kurs2);
+            Console.WriteLine(kurs3);
+            Console.WriteLine(kurs4);
+            Console.WriteLine(kurs5);
+            Console.WriteLine("");
+            //statik şekilde yazınca kursları göstermek için ayrı ayrı console.writeline kullanmamız gerekecekti,
+            //bunun yerine dinamik olması için liste de tanımlayıp döngüleri kullandık.
+            //kursları tek tek değişkenlere tanımlamak yerine tek değişken içerisinde liste halinde tanımladık ve döngü ile ekranda gösterdik.
+
+            //array - dizi
+            //string array
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+                "Programlamaya Başlangıç İçin Temel Kurs", "Java", "Python", "C#" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Bitti");
+
+            //diğer bir döngü yöntemi ise foreach' tur.
+            //foreach: dizi temelli yapıları tek tek dönmeye yarıyor
+
+            foreach (string kurs in kurslar)
+            //burada ki kurs allias 'tır yani takma isim. kurs yerine başka birşey de yazılabilirdi.
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - footer");
+
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güveniği-C# yazılım dilinde değişkenlerin veri tipini istemesi.
+            //Do not repeat yourself - kendinin tekrarlama
+            //kategori etiketi aslında bir değer tutucudur, aliastır.
+
+            //string kategoriEtiketi = "Kategori";
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;     //bool veri tipi kontrol işlemlerinde kull. true ve false değerleri atanır.
+                                                  //gerçek hayatta true kısmı bir veri kaynağından gelir. Fonksiyona bağlı olur.
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            //tab tab
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Butonunu Göster");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonunu Göster");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
